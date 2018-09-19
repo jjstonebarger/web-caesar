3ccdf5d8da6d9f1a37b5bea2e550ee1e8650f419
Justin Stonebarger <jjstonebarger@gmail.com>
2018-09-19 17:11:29 -0600

    Readability fix

--- main.py
+++ main.py
@@ -10,18 +10,18 @@ form = """
         <head>
             <style>
                 form {{
-                background-color: #eee;
-                padding: 20px;
-                margin: 0 auto;
-                width: 540px;
-                font: 16px sans-serif;
-                border-radius: 10px;
-            }}
-            textarea {{
-                margin: 10px 0;
-                width: 540px;
-                height: 120px;
-            }}
+                    background-color: #eee;
+                    padding: 20px;
+                    margin: 0 auto;
+                    width: 540px;
+                    font: 16px sans-serif;
+                    border-radius: 10px;
+                }}
+                textarea {{
+                    margin: 10px 0;
+                    width: 540px;
+                    height: 120px;
+                }}
             </style>
         </head>
         <body>
@@ -36,7 +36,7 @@ form = """
 
 @app.route("/")
 def index():
-    return form.format(...)
+    return form.format("")
 
 @app.route("/", methods=['POST'])
 def encrypt():
